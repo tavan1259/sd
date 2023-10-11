78e3812d30ab32eec811a6a046f8f32c193a7d33
Man <100039834+tavan1259@users.noreply.github.com>
2023-10-11 17:15:14 +0700

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,34 @@
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello"
+
+@app.route('/hello/<name>', methods=['GET'])
+def hello(name):
+    return "Hello, " + str(name)
+
+
+@app.route('/calculate/<num1>/<num2>', methods=['GET'])
+def calculate(num1, num2):
+    try:
+        num1 = eval(num1)
+        num2 = int(num2)
+
+        results = {
+                'plus' : num1 + num2,
+                'minus' : num1 - num2,
+                'multiply': num1 * num2,
+                'divide' : num1/num2
+            }
+    except:
+        results = { 'error_msg' : 'inputs must be numbers' }
+
+    return jsonify(results)
+
+
+if __name__ == '__main__':
+    app.run()
